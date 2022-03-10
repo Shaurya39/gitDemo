ea2e5dd127afe69d8dbf15433e1a31db29290b77
Shaurya39 <shauryashakti3@gmail.com>
2022-03-10 22:03:42 +0530

    why

--- src/main/java/qaclickacademy/Mavenjava/App.java
+++ src/main/java/qaclickacademy/Mavenjava/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "why" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "i am checking");
